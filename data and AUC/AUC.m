32429953870bc76e913009d3eb9e8ea55cbe362d
Zhaoyue Wu <95431391+WuZhaoyue@users.noreply.github.com>
2022-08-21 22:03:16 +0200

    Update AUC.m

--- data and AUC/AUC.m
+++ data and AUC/AUC.m
@@ -1,13 +1,4 @@
 function [PF, PD, area]=AUC(mask, E)
-anomaly_map = logical(double(mask)>=1);
-normal_map = logical(double(mask)==0);
-r_max = max(E(:));
-taus = linspace(0, r_max, 5000);
-for index2 = 1:length(taus)
-    tau = taus(index2);
-    anomaly_map_rx = (E> tau);
-    PF(index2) = sum(anomaly_map_rx & normal_map)/sum(normal_map);
-    PD(index2) = sum(anomaly_map_rx & anomaly_map)/sum(anomaly_map);
-end
-area =  sum((PF(1:end-1)-PF(2:end)).*(PD(2:end)+PD(1:end-1))/2);
+[PD,PF,~] = roc(mask,E);
+area =  abs(sum((PF(1:end-1)-PF(2:end)).*(PD(2:end)+PD(1:end-1))/2));
 end
