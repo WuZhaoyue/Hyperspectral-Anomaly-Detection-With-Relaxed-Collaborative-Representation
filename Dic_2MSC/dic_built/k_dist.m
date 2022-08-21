941b1d29e69ec367883d3d08cde0b179a2bfa145
Zhaoyue Wu <95431391+WuZhaoyue@users.noreply.github.com>
2022-08-21 21:34:33 +0200

    Update k_dist.m

--- Dic_2MSC/dic_built/k_dist.m
+++ Dic_2MSC/dic_built/k_dist.m
@@ -1,17 +1,11 @@
 function k_dist(A, k)
+
 numData=size(A, 1);
-
-Kdist=zeros(numData,1);
-[~,Dist]=knnsearch(A(2:numData,:),A(1,:), 'K', k);
-Kdist(1)=Dist(k);
-% Kdist2(1)=Dist(k2);
-
-for i=2:size(A,1)
-    [~,Dist] = knnsearch(A([1:i-1,i+1:numData],:),A(i,:), 'K', k);
-    Kdist(i)=Dist(k);
-end
-
+dist = pdist2(A,A);
+kMatrix=sort(dist, 'ascend');
+Kdist=kMatrix(k,:);
 [sortKdist,~]=sort(Kdist,'descend');
+
 distX=(1:numData)';
 figure
 plot(distX,sortKdist,'r+-','LineWidth',1);
@@ -21,6 +15,4 @@ set(text,'Units','Normalized','Position',[-0.13,0.5,0]);
 set(text1,'Units','Normalized','Position',[0.5,-0.12,0]);
 set(gcf,'position',[100 100 300 300]); 
 set (gca,'position',[0.13 0.14 0.81 0.82]);
-% axis([0 13000 0 6.2])
 grid on;
-
